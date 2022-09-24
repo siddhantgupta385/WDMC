d28caa30c61ab61670afaa8fcf861a6fd11d7e40
Siddhant Gupta <siddhantgupta384@gmail.com>
2022-09-24 18:02:14 +0530

    clubs data added

--- Backend/models/Club.model.js
+++ Backend/models/Club.model.js
@@ -4,16 +4,16 @@ const Schema = mongoose.Schema;
 
 
 const ClubSchema = new Schema({
-  ClubName: { type: String, required: true },
-  ClubWebsite: { type: String, required: false },
-  ClubImg:  [{type: String, required: true}],
-  ClubDesc :{type:String,required:true},
-  ClubExtraContent :{type:String,required:false},
-  ClubMemberNames :[{type:String,required:true}],
-  ClubMemberDesign :[{type:String,required:true}],
-  date: { type: Date, required: false  , default: Date.now},
+    ClubName: { type: String, required: true },
+    ClubWebsite: { type: String, required: false },
+    ClubImg: [{ type: String, required: false }],
+    ClubDesc: { type: String, required: false },
+    ClubExtraContent: { type: String, required: false },
+    // ClubMemberNames :[{type:String,required:true}],
+    // ClubMemberDesign :[{type:String,required:true}],
+    date: { type: Date, required: false, default: Date.now },
 }, {
-  timestamps: true,
+    timestamps: true,
 });
 
 const Club = mongoose.model('Club', ClubSchema);
--- Backend/routes/Club.js
+++ Backend/routes/Club.js
@@ -12,28 +12,28 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
-  });
+});
 const upload = multer({ storage: storage });
 
 //fetching all the club info
-router.get('/get',(req, res) => {
+router.get('/get', (req, res) => {
     ClubModel.find()
         .then(club => res.json(club))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
 //dynamic end point for different clubs 
-router.get("/:club_name",(req,res)=>{
+router.get("/:club_name", (req, res) => {
     const club_name = req.params.club_name
-    res.render("index.hbs",{name:club_name});  
+    res.render("index.hbs", { name: club_name });
 });
 
 
 //fetching club info with its name 
-router.get('/get/:club_name',(req, res) => {
+router.get('/get/:club_name', (req, res) => {
     const club_name = req.params.club_name
     console.log(club_name)
-    ClubModel.find({ClubName:club_name})
+    ClubModel.find({ ClubName: club_name })
         .then(club => res.json(club))
         .catch(err => res.status(400).json('Error: ' + err));
 });
@@ -44,27 +44,27 @@ router.post('/add', upload.array('ClubImg', 10), (req, res, next) => {
 
     //required attributes
     var obj = {
-      ClubName: req.body.ClubName,
-      ClubDesc: req.body.ClubDesc,
-      ClubImg:[],
-      ClubMemberNames:[],
-      ClubMemberDesign:[],
-    }
-    //optional attributes
-    if(req.body.ClubWebsite != undefined){
+            ClubName: req.body.ClubName,
+            ClubDesc: req.body.ClubDesc,
+            ClubImg: [],
+            //   ClubMemberNames:[],
+            //   ClubMemberDesign:[],
+        }
+        //optional attributes
+    if (req.body.ClubWebsite != undefined) {
         obj.ClubWebsite = req.body.ClubWebsite;
     }
-    if(req.body.ClubExtraContent != undefined){
+    if (req.body.ClubExtraContent != undefined) {
         obj.ClubExtraContent = req.body.ClubExtraContent;
     }
-    if(req.body.ClubMemberNames != undefined && req.body.ClubMemberNames.length != 0){
-            obj.ClubMemberNames = req.body.ClubMemberNames;
-    }
-    if(req.body.ClubMemberDesign != undefined && req.body.ClubMemberDesign.length != 0){
-            obj.ClubMemberDesign = req.body.ClubMemberDesign;
-    }
+    // // if(req.body.ClubMemberNames != undefined && req.body.ClubMemberNames.length != 0){
+    // // obj.ClubMemberNames = req.body.ClubMemberNames;
+    // }
+    // // if(req.body.ClubMemberDesign != undefined && req.body.ClubMemberDesign.length != 0){
+    // // obj.ClubMemberDesign = req.body.ClubMemberDesign;
+    // }
 
-    for( let i =0 ; i< req.files.length ; i++){
+    for (let i = 0; i < req.files.length; i++) {
         obj.ClubImg.push(path.join(__dirname + '\\..\\uploads\\club\\' + req.files[i].filename))
     }
     // console.log(obj)
@@ -72,13 +72,12 @@ router.post('/add', upload.array('ClubImg', 10), (req, res, next) => {
         if (err) {
             console.log(err);
             res.send('Error!');
-        }
-        else {
+        } else {
             item.save();
             res.send('club added successfully!');
         }
     });
 
-  });
+});
 
 module.exports = router;
