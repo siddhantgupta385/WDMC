7d845e972628c9cb121744910b0469f13b0188d3
Siddhant Gupta <siddhantgupta384@gmail.com>
2022-09-25 14:31:32 +0530

    ----

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ var path = require('path');
 const handlebars = require("express-handlebars");
 const clubRouter = require('./routes/Club.js');
 require('dotenv').config();
+const hbs = require('hbs')
 
 const app = express();
 const port = process.env.PORT || 6000;
@@ -16,45 +17,48 @@ app.use(express.json());
 
 
 // mongo db connection
-    const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI;
 
-    mongoose.connect(uri, { 
-      useNewUrlParser: true, 
-      useUnifiedTopology: true }
-    );
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-    const connection = mongoose.connection;
-    connection.once('open', () => {
-      console.log("MongoDB database connection established successfully");
-    })
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
 // Setting Handlebars for frontend 
-    app.engine('hbs',handlebars.engine({
-      extname:'hbs',
-      defaultLayout:false
-    }));
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    defaultLayout: false
+}));
 
-    app.set('view engine','hbs');
-    app.set('views',path.join(__dirname,'../frontend/'));
-    app.use(express.static(path.join(__dirname,"../frontend/")));
+const partialsPath = path.join(__dirname, "../frontend/partials");
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../frontend/'));
+app.use(express.static(path.join(__dirname, "../frontend/")));
+hbs.registerPartials(partialsPath)
 
-    Handlebars.registerHelper('ifNotEquals', function(arg1, arg2, options) {
-      return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
-    });
+
+Handlebars.registerHelper('ifNotEquals', function(arg1, arg2, options) {
+    return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+});
 
 
 // End Points
-    app.use('/club', clubRouter);
-    app.get("/Club_form",(req,res)=>{
-      res.render("club_form.hbs")
-    });
-    app.get("/", (req, res) =>{
-      res.render("main.hbs")
-    })
-    app.listen(port, () => {
-        console.log(`Server is running on port: ${port}`);
-    });
+app.use('/club', clubRouter);
+app.get("/Club_form", (req, res) => {
+    res.render("club_form.hbs")
+});
+app.get("/", (req, res) => {
+    res.render("main.hbs")
+})
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
